d1ef16984d3d695de0f3c0af3bd08f47be2bfc0c
Shiyab <shiyabputtur3@gmail.com>
2024-12-02 11:43:44 +0530

    first-commit

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,20 +1,23 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+// require('dotenv').config();
+
 module.exports = {
   solidity: {
-    version: '0.8.9',
-    defaultNetwork: 'goerli',
-    networks: {
-      hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
-        accounts: [`0x${process.env.PRIVATE_KEY}`]
-      }
-    },
+    version: '0.8.20',
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  defaultNetwork: 'sepolia',
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: 'https://rpc.ankr.com/eth_sepolia/026dfc9d539694c17d178e1b0ecc2ea3707f440d110ceea20c49f169d87254d1',
+      //accounts: [`0x${process.env.PRIVATE_KEY}`]
+      accounts: ['0x37c64bbbf8232b399e48026edf8f8c2e4679127dc72141d66db0923d13619ec1']
     },
   },
 };
